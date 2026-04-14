e1ed0a81672ae48d6e404e06ab9d39e6bb834ba2
Créopus <alonzothebest@gmail.com>
2026-04-14 12:44:30 +0200

    Fix: ajout route explicite /demo/ pour servir les fichiers de démonstration

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/demo/:file', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'demo', req.params.file))
 })
 
 app.listen(PORT, () => {
